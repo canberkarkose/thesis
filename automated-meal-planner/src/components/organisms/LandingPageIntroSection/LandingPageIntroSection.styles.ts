d7e94ff86cee858fbc8f7acb0954ef4af1988efa
canberkarkose <canberk.arkose@outlook.com>
2024-04-08 16:19:15 +0200

    feat: intro in landing page

--- automated-meal-planner/src/components/organisms/LandingPageIntroSection/LandingPageIntroSection.styles.ts
+++ automated-meal-planner/src/components/organisms/LandingPageIntroSection/LandingPageIntroSection.styles.ts
@@ -1,5 +1,15 @@
 import { Box } from '@mui/material';
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+import { ReactComponent as IntroPlateSVG } from '../../../assets/introPlate.svg';
+import { ReactComponent as biryaniLogoSVG } from '../../../assets/biryani.svg';
+import { ReactComponent as burgerLogoSVG } from '../../../assets/burger.svg';
+import { ReactComponent as dietLogoSVG } from '../../../assets/diet.svg';
+import { ReactComponent as dinnerLogoSVG } from '../../../assets/dinner.svg';
+import { ReactComponent as fruitsLogoSVG } from '../../../assets/fruits.svg';
+import { ReactComponent as ramenLogoSVG } from '../../../assets/ramen.svg';
+import { ReactComponent as saladLogoSVG } from '../../../assets/salad.svg';
 
 export const IntroContainer = styled(Box)`
   display: flex;
@@ -28,12 +38,101 @@ export const ImageContainer = styled(Box)`
   max-width: 500px;
   height: 250px;
   margin-left: 20px;
-  border: 1px solid #000;
-  background-color: #fff;
+  position: relative;
 
   @media (max-width: 768px) {
     max-width: 100%;
     margin-left: 0;
     margin-top: 20px;
   }
+`;
+
+const baseSvgStyle = `
+  width: 100px;
+  height: 100px;
+`;
+
+export const StyledBiryani = styled(biryaniLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledBurger = styled(burgerLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledDiet = styled(dietLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledDinner = styled(dinnerLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledFruits = styled(fruitsLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledRamen = styled(ramenLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledSalad = styled(saladLogoSVG)`
+  ${baseSvgStyle}
+`;
+
+export const StyledIntroPlate = styled(IntroPlateSVG)`
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 175px;
+  height: 200px;
+  filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 1));
+`;
+
+const moveAcross = keyframes`
+  0%, 10%, 100% {
+    left: -100px;
+    top: 35%;
+    transform: scale(1) translateY(-50%);
+    opacity: 0;
+  }
+  40% {
+    left: calc(50% - 52px);
+    top: 35%;
+    transform: scale(1) translateY(-50%);
+    opacity: 1;
+  }
+  50% {
+    left: calc(50% - 52px);
+    top: 40%;
+    transform: scale(1.4) translateY(-50%);
+    filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 1));
+    opacity: 1;
+  }
+  60% {
+    left: calc(50% - 52px);
+    top: 35%;
+    transform: scale(1) translateY(-50%);
+    opacity: 1;
+  }
+  70% {
+    left: calc(50% - 52px);
+    top: 35%;
+    transform: scale(1) translateY(-50%);
+    opacity: 1;
+  }
+  90%, 95% {
+    left: 100%;
+    top: 35%;
+    transform: scale(1) translateY(-50%);
+    opacity: 0;
+  }
+`;
+
+export const AnimatedIcon = styled(Box)`
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  animation: ${moveAcross} 6s linear;
 `;
--- automated-meal-planner/src/components/organisms/LandingPageIntroSection/LandingPageIntroSection.tsx
+++ automated-meal-planner/src/components/organisms/LandingPageIntroSection/LandingPageIntroSection.tsx
@@ -1,36 +1,71 @@
 import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
+
 import { Space } from '../../atoms/Space/Space';
 
 import {
   IntroContainer,
   ImageContainer,
   TextContainer,
+  StyledIntroPlate,
+  AnimatedIcon,
+  StyledBiryani,
+  StyledBurger,
+  StyledDiet,
+  StyledDinner,
+  StyledFruits,
+  StyledRamen,
+  StyledSalad
 } from './LandingPageIntroSection.styles';
 
-export const LandingPageIntroSection = () => (
-  <IntroContainer>
-    <TextContainer>
-      <Typography variant='h4' color='primary.dark' fontSize='30px' fontWeight='bold' gutterBottom>
-        Transform Your Eating Habits with Bite by Byte
-      </Typography>
-      <Typography variant='body1' color='text.primary' gutterBottom>
-        Simplify your meal planning with Bite by Byte.
-        Personalize your diet, save time, and achieve your nutrition goals effortlessly.
-      </Typography>
-      <Space s16 />
-      <Button variant='contained' size='large' color='primary' component={Link} to='/register' sx={{ marginBottom: '10px' }}>
-        Sign up now
-      </Button>
-      <Typography variant='subtitle2' sx={{ fontSize: '0.7rem', ml: 0.5 }}>
-        Already a member?
-        {' '}
-        <Link to='/login'>Sign in</Link>
-      </Typography>
-    </TextContainer>
-    <ImageContainer>
-      <Typography variant='h5' sx={{ mt: 13, ml: 2 }}>Placeholder for Simple Animation</Typography>
-    </ImageContainer>
-  </IntroContainer>
-);
+const icons = [
+  StyledBiryani,
+  StyledBurger,
+  StyledDiet,
+  StyledDinner,
+  StyledFruits,
+  StyledRamen,
+  StyledSalad
+];
+
+export const LandingPageIntroSection = () => {
+  const [currentIconIndex, setCurrentIconIndex] = useState(0);
+
+  const handleAnimationEnd = () => {
+    setCurrentIconIndex((currentIconIndex + 1) % icons.length);
+    // eslint-disable-next-line no-console
+    console.log('Animation ended, currentIconIndex:', currentIconIndex);
+  };
+
+  const CurrentIcon = icons[currentIconIndex];
+  return (
+    <IntroContainer>
+      <TextContainer>
+        <Typography variant='h4' color='primary.dark' fontSize='30px' fontWeight='bold' gutterBottom>
+          Transform Your Eating Habits with Bite by Byte
+        </Typography>
+        <Typography variant='body1' color='text.primary' gutterBottom>
+          Simplify your meal planning with Bite by Byte.
+          Personalize your diet, save time, and achieve your nutrition goals effortlessly.
+        </Typography>
+        <Space s16 />
+        <Button variant='contained' size='large' color='primary' component={Link} to='/register' sx={{ marginBottom: '10px' }}>
+          Sign up now
+        </Button>
+        <Typography variant='subtitle2' sx={{ fontSize: '0.7rem', ml: 0.5 }}>
+          Already a member?
+          {' '}
+          <Link to='/login'>Sign in</Link>
+        </Typography>
+      </TextContainer>
+      <ImageContainer>
+        <StyledIntroPlate />
+        <AnimatedIcon key={currentIconIndex} onAnimationEnd={handleAnimationEnd}>
+          <CurrentIcon />
+        </AnimatedIcon>
+      </ImageContainer>
+    </IntroContainer>
+  );
+};
